ff743603aea38cba4fdf24419fc39a67f089394e
杨德黎 <deli.yang@hand-china.com>
2021-02-22 13:53:23 +0800

    [fix]调整建表 SQL 文件

--- delayed_job.sql
+++ delayed_job.sql
@@ -11,27 +11,27 @@
  Target Server Version : 80021
  File Encoding         : 65001
 
- Date: 27/01/2021 16:33:43
+ Date: 22/02/2021 13:50:30
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for delayed_job
+-- Table structure for DELAYED_JOB
 -- ----------------------------
-DROP TABLE IF EXISTS `delayed_job`;
-CREATE TABLE `delayed_job` (
-  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `status` varchar(255) COLLATE utf8_bin NOT NULL COMMENT '状态',
-  `group` varchar(255) COLLATE utf8_bin NOT NULL COMMENT '组',
-  `code` varchar(255) COLLATE utf8_bin NOT NULL COMMENT '编码',
-  `job_class` varchar(255) COLLATE utf8_bin NOT NULL COMMENT 'job 的 class ',
-  `date` varchar(50) COLLATE utf8_bin NOT NULL COMMENT '触发时间',
-  `name` varchar(255) COLLATE utf8_bin NOT NULL COMMENT '名称',
-  `description` varchar(255) COLLATE utf8_bin DEFAULT NULL COMMENT '描述',
-  `param` text COLLATE utf8_bin COMMENT '参数',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=147 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+DROP TABLE IF EXISTS `DELAYED_JOB`;
+CREATE TABLE `DELAYED_JOB` (
+  `ID` varchar(50) COLLATE utf8_bin NOT NULL COMMENT '主键',
+  `STATUS` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '状态',
+  `GROUP` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '组',
+  `CODE` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '编码',
+  `JOB_CLASS` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT 'job 的 class ',
+  `DATE` timestamp NOT NULL COMMENT '触发时间',
+  `NAME` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '名称',
+  `DESCRIPTION` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '描述',
+  `PARAM` text CHARACTER SET utf8 COLLATE utf8_bin COMMENT '参数',
+  PRIMARY KEY (`ID`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 SET FOREIGN_KEY_CHECKS = 1;
